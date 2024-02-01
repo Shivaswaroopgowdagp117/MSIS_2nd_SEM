0c4c2f02b779b378310daf3d7b4cb832363656df
SHIVA SWAROOP GOWDA G P <143502801+Shivaswaroopgowdagp117@users.noreply.github.com>
2024-02-01 11:16:17 +0000

    Commit

---
+++ MCO/prgm1.c
@@ -0,0 +1,14 @@
+/*Write a simple C program to generate a random number
+without initializing the seed for the random number
+generator {Within a span of "one second", if you keep
+running the program multiple times, you should see the same
+random number output}*/
+
+
+#include <stdio.h>
+int main()
+{
+    int random_number = rand();
+    printf("Random number (without seeding):%d\n",random_number);
+    return 0;
+}
