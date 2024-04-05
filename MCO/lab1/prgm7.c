3dbeef0a36a3f6c43a8b71e841bed8cef1de3311
SHIVA SWAROOP GOWDA G P <143502801+Shivaswaroopgowdagp117@users.noreply.github.com>
2024-04-05 13:40:58 +0000

    Commit

--- MCO/lab1/prgm7.c
+++ MCO/lab1/prgm7.c
@@ -6,9 +6,34 @@ matrix to the screen.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main()
-{
-    int matrix=[row][column];
-    for(int i=0;i<row;i++)
-        for(int j=0;j<column;j++)
+
+#define ROWS 5
+#define COLS 10
+
+void populateMatrix(int matrix[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            matrix[i][j] = rand() % 10 + 1;
+        }
+    }
+}
+
+void displayMatrix(int matrix[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[ROWS][COLS];
+
+    srand(time(0)); // Seed for random number generation
+
+    populateMatrix(matrix);
+    displayMatrix(matrix);
+
+    return 0;
 }
